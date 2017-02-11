90338de2c0e30fce6218e8cbe821de7d2fe82570
Kirill Antikov <crazytraffic@gmail.com>
2017-02-11 08:49:55 +0000

    solve 12 puzzle both parts

--- 12/second.js
+++ 12/second.js
@@ -3,4 +3,31 @@ var input = fs.readFileSync("input").toString().trim();
 
 var answer = 0;
 
-console.log(answer);
+function objParse (obj) {
+    var count = 0;
+    var red = false;
+    
+    for (var key in obj) {
+        if (typeof obj[key] == "object" ) {
+            count += objParse(obj[key]);
+        }
+        else {
+            if (typeof obj[key] == "number") {
+                count += obj[key];
+            }
+            else if (obj[key] == "red") {
+                red = true;
+            }
+        }
+    }
+    
+    if (!(obj instanceof Array) && (obj instanceof Object) && red) {
+        return 0;
+    }
+    
+    return count;
+}
+
+var input = JSON.parse(input);
+
+console.log(objParse(input));
