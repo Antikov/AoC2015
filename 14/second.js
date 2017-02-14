d0c67e992e8239048e421f4f280a2cc4e070cd8c
Kirill Antikov <crazytraffic@gmail.com>
2017-02-14 10:53:53 +0000

    solve 1st part 14 puzzle

---
+++ 14/second.js
@@ -0,0 +1,36 @@
+var fs = require("fs");
+var input = fs.readFileSync("input.1").toString().trim();
+
+var time = 2503;
+time = 1000;
+var reinder = [];
+
+function emulate(speed, speedTime, restTime) {
+    var i = 0;
+    var j = 0;
+    var distance = 0;
+    while (i < time) {
+        if (j < speedTime) {
+            j++;
+            i++;
+            distance++;
+        } else {
+            i += restTime;
+            j = 0;
+        }
+    }
+    
+    return distance;
+}
+
+input.split("\n").forEach(function(data) {
+    var value = data.match(/\d+/g);
+    var name = data.split(" ")[0];
+    reinder.push([name, emulate(+value[0], +value[1], +value[2])]);
+});
+
+reinder.sort(function(a, b) {
+    return b[1] - a[1];
+})
+
+console.log(reinder[0]);
